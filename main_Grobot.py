831af9e113213940cd2ff8f4267e2e844b432cd9
superpingu <arnaudbonetti@me.com>
2017-05-30 22:52:58 +0200

    tests with jack OK

--- main_Grobot.py
+++ main_Grobot.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import motion
-import I2C_bus
-from starting_block import add_jack_and_delay
+import motion                           #from libmotors
+from I2C_bus import I2C_bus             #from libAX12/pythonBinding
+from starting_block import add_jack_and_delay, time_elapsed, manage_time_elapsed
 # the robot is "constructed" in structure_Grobot.py
 from structure_Grobot import *
 
+from time import sleep
 
 ############################ TOP LEVEL ACTION DEFINITION #######################
 # remainder: definition != execution
 
 robot.add_sequence("main_sequence")
 
+# IMPORTANT : programs a global stop on the raspi, ie no more actions will be done
+# NOTE : this is not sufficient !!! a stop command must be send to the STM
+# some cleaning must also be done: stop AX12, ...
+robot.add_parallel(time_elapsed, [10, lambda: manage_time_elapsed(robot)], False)
+robot.wait()
+
+robot.add_parallel(lambda a: sleep(20), []) #bidon juste pour etre bloquant
+robot.wait()
 
 robot.sequence_done()
 
@@ -36,6 +45,7 @@ gpio.set_pin_mode(jack_pin, gpio.OUTPUT) #easier to test with hand (gpio write 5
 # ie we don't test with the real jack, we just simulate it
 #with the real jack, it must be gpio.INPUT )
 
+# delay = 10 = maximum time the robot waits before aborting
 manage_jack = add_jack_and_delay(robot, 30)
 
 gpio.assign_callback_on_gpio_down(24, lambda: manage_jack(False))
